2c51f9bf5a68c4692c36081874899ecaadcc818e
Behod Halil <behzoddev@gmail.com>
2022-07-06 20:54:31 +0900

    Add subcategory variable to NoteCategory

--- data-caching/src/main/java/uz/behzoddev/data_caching/entities/NoteCategory.kt
+++ data-caching/src/main/java/uz/behzoddev/data_caching/entities/NoteCategory.kt
@@ -1,11 +1,13 @@
 package uz.behzoddev.data_caching.entities
 
+import java.time.Instant
+
 data class NoteCategory(
     var id: Int = 0,
     var title: String = "",
     var description: Note? = null,
-    var timestamp: Long = System.currentTimeMillis(),
+    var timestamp: Long = Instant.now().epochSecond,
     var count: Long = 0L,
-    var type: String,
-    var subCategory: List<NoteSubCategory>? = null
+    var type: String = "",
+    var subCategory: List<NoteSubCategory> = listOf()
 )
