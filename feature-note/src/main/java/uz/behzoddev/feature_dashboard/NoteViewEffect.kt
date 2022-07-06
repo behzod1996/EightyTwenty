b40004f71269f74a3d14a76b40540cb2906fcd9c
Behod Halil <behzoddev@gmail.com>
2022-07-06 20:52:59 +0900

    Add effect objects to NoteViewEffect.kt

--- feature-note/src/main/java/uz/behzoddev/feature_dashboard/NoteViewEffect.kt
+++ feature-note/src/main/java/uz/behzoddev/feature_dashboard/NoteViewEffect.kt
@@ -1,4 +1,8 @@
 package uz.behzoddev.feature_dashboard
 
 sealed class NoteViewEffect {
+    object NewNoteViewEffect: NoteViewEffect()
+    object CategoryViewEffect: NoteViewEffect()
+    object DetailViewEffect: NoteViewEffect()
+    object SearchViewEffect: NoteViewEffect()
 }
