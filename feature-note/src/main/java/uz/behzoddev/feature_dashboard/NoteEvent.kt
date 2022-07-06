4280ca0075c4049611da417d8be1de8cc2925460
Behod Halil <behzoddev@gmail.com>
2022-07-06 20:54:41 +0900

    Add events to NoteEvent.kt

--- feature-note/src/main/java/uz/behzoddev/feature_dashboard/NoteEvent.kt
+++ feature-note/src/main/java/uz/behzoddev/feature_dashboard/NoteEvent.kt
@@ -1,4 +1,8 @@
 package uz.behzoddev.feature_dashboard
 
 sealed class NoteEvent {
+    object NewNoteViewEffect: NoteEvent()
+    object CategoryViewEffect: NoteEvent()
+    object DetailViewEffect: NoteEvent()
+    object SearchViewEffect: NoteEvent()
 }
