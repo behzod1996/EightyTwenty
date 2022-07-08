9688f27c0b8ee6e932f26cfffcb591c0abb36a2c
Behod Halil <behzoddev@gmail.com>
2022-07-08 20:49:45 +0900

    Add PrimaryTypeExtensions.kt

---
+++ core-ui/src/main/java/uz/behzoddev/common_ui/PrimaryTypeExtensions.kt
@@ -0,0 +1,10 @@
+package uz.behzoddev.common_ui
+
+val String.Companion.Empty: String
+    get() = ""
+
+val Long.Companion.Empty: Long
+    get() = 0L
+
+val Int.Companion.Empty: Int
+    get() = 0
