14c2e1d4563c5c35984c3e16bc64641469b4da87
Behod Halil <behzoddev@gmail.com>
2022-07-08 21:03:09 +0900

    Update settings

--- data-caching/build/tmp/kapt3/stubs/debug/uz/behzoddev/data_caching/entities/NoteCategory.java
+++ data-caching/build/tmp/kapt3/stubs/debug/uz/behzoddev/data_caching/entities/NoteCategory.java
@@ -1,26 +1,28 @@
 package uz.behzoddev.data_caching.entities;
 
-import java.lang.System;
+import android.os.Parcelable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import kotlinx.parcelize.Parcelize;
+import java.time.Instant;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u00008\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\t\n\u0002\b\u0003\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\"\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0086\b\u0018\u00002\u00020\u0001BS\u0012\b\b\u0002\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0005\u0012\n\b\u0002\u0010\u0006\u001a\u0004\u0018\u00010\u0007\u0012\b\b\u0002\u0010\b\u001a\u00020\t\u0012\b\b\u0002\u0010\n\u001a\u00020\t\u0012\u0006\u0010\u000b\u001a\u00020\u0005\u0012\u0010\b\u0002\u0010\f\u001a\n\u0012\u0004\u0012\u00020\u000e\u0018\u00010\r\u00a2\u0006\u0002\u0010\u000fJ\t\u0010(\u001a\u00020\u0003H\u00c6\u0003J\t\u0010)\u001a\u00020\u0005H\u00c6\u0003J\u000b\u0010*\u001a\u0004\u0018\u00010\u0007H\u00c6\u0003J\t\u0010+\u001a\u00020\tH\u00c6\u0003J\t\u0010,\u001a\u00020\tH\u00c6\u0003J\t\u0010-\u001a\u00020\u0005H\u00c6\u0003J\u0011\u0010.\u001a\n\u0012\u0004\u0012\u00020\u000e\u0018\u00010\rH\u00c6\u0003JY\u0010/\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\n\b\u0002\u0010\u0006\u001a\u0004\u0018\u00010\u00072\b\b\u0002\u0010\b\u001a\u00020\t2\b\b\u0002\u0010\n\u001a\u00020\t2\b\b\u0002\u0010\u000b\u001a\u00020\u00052\u0010\b\u0002\u0010\f\u001a\n\u0012\u0004\u0012\u00020\u000e\u0018\u00010\rH\u00c6\u0001J\u0013\u00100\u001a\u0002012\b\u00102\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u00103\u001a\u00020\u0003H\u00d6\u0001J\t\u00104\u001a\u00020\u0005H\u00d6\u0001R\u001a\u0010\n\u001a\u00020\tX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0010\u0010\u0011\"\u0004\b\u0012\u0010\u0013R\u001c\u0010\u0006\u001a\u0004\u0018\u00010\u0007X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0014\u0010\u0015\"\u0004\b\u0016\u0010\u0017R\u001a\u0010\u0002\u001a\u00020\u0003X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0018\u0010\u0019\"\u0004\b\u001a\u0010\u001bR\"\u0010\f\u001a\n\u0012\u0004\u0012\u00020\u000e\u0018\u00010\rX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001c\u0010\u001d\"\u0004\b\u001e\u0010\u001fR\u001a\u0010\b\u001a\u00020\tX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b \u0010\u0011\"\u0004\b!\u0010\u0013R\u001a\u0010\u0004\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\"\u0010#\"\u0004\b$\u0010%R\u001a\u0010\u000b\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b&\u0010#\"\u0004\b\'\u0010%\u00a8\u00065"}, d2 = {"Luz/behzoddev/data_caching/entities/NoteCategory;", "", "id", "", "title", "", "description", "Luz/behzoddev/data_caching/entities/Note;", "timestamp", "", "count", "type", "subCategory", "", "Luz/behzoddev/data_caching/entities/NoteSubCategory;", "(ILjava/lang/String;Luz/behzoddev/data_caching/entities/Note;JJLjava/lang/String;Ljava/util/List;)V", "getCount", "()J", "setCount", "(J)V", "getDescription", "()Luz/behzoddev/data_caching/entities/Note;", "setDescription", "(Luz/behzoddev/data_caching/entities/Note;)V", "getId", "()I", "setId", "(I)V", "getSubCategory", "()Ljava/util/List;", "setSubCategory", "(Ljava/util/List;)V", "getTimestamp", "setTimestamp", "getTitle", "()Ljava/lang/String;", "setTitle", "(Ljava/lang/String;)V", "getType", "setType", "component1", "component2", "component3", "component4", "component5", "component6", "component7", "copy", "equals", "", "other", "hashCode", "toString", "data-caching_debug"})
-public final class NoteCategory {
+@androidx.room.Entity(tableName = "note_category")
+@kotlinx.parcelize.Parcelize()
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000F\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\t\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u001b\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0000\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u0087\b\u0018\u00002\u00020\u0001B=\u0012\b\b\u0002\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0005\u0012\b\b\u0002\u0010\u0006\u001a\u00020\u0007\u0012\b\b\u0002\u0010\b\u001a\u00020\u0007\u0012\u000e\b\u0002\u0010\t\u001a\b\u0012\u0004\u0012\u00020\u000b0\n\u00a2\u0006\u0002\u0010\fJ\t\u0010\u001f\u001a\u00020\u0003H\u00c6\u0003J\t\u0010 \u001a\u00020\u0005H\u00c6\u0003J\t\u0010!\u001a\u00020\u0007H\u00c6\u0003J\t\u0010\"\u001a\u00020\u0007H\u00c6\u0003J\u000f\u0010#\u001a\b\u0012\u0004\u0012\u00020\u000b0\nH\u00c6\u0003JA\u0010$\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00072\b\b\u0002\u0010\b\u001a\u00020\u00072\u000e\b\u0002\u0010\t\u001a\b\u0012\u0004\u0012\u00020\u000b0\nH\u00c6\u0001J\t\u0010%\u001a\u00020\u0003H\u00d6\u0001J\u0013\u0010&\u001a\u00020\'2\b\u0010(\u001a\u0004\u0018\u00010)H\u00d6\u0003J\t\u0010*\u001a\u00020\u0003H\u00d6\u0001J\t\u0010+\u001a\u00020\u0005H\u00d6\u0001J\u0019\u0010,\u001a\u00020-2\u0006\u0010.\u001a\u00020/2\u0006\u00100\u001a\u00020\u0003H\u00d6\u0001R\u001a\u0010\b\u001a\u00020\u0007X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\u000e\"\u0004\b\u000f\u0010\u0010R\u001e\u0010\u0002\u001a\u00020\u00038\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0011\u0010\u0012\"\u0004\b\u0013\u0010\u0014R \u0010\t\u001a\b\u0012\u0004\u0012\u00020\u000b0\nX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0015\u0010\u0016\"\u0004\b\u0017\u0010\u0018R\u001a\u0010\u0006\u001a\u00020\u0007X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0019\u0010\u000e\"\u0004\b\u001a\u0010\u0010R\u001a\u0010\u0004\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001b\u0010\u001c\"\u0004\b\u001d\u0010\u001e\u00a8\u00061"}, d2 = {"Luz/behzoddev/data_caching/entities/NoteCategory;", "Landroid/os/Parcelable;", "id", "", "title", "", "timestamp", "", "count", "subCategory", "", "Luz/behzoddev/data_caching/entities/NoteSubCategory;", "(ILjava/lang/String;JJLjava/util/List;)V", "getCount", "()J", "setCount", "(J)V", "getId", "()I", "setId", "(I)V", "getSubCategory", "()Ljava/util/List;", "setSubCategory", "(Ljava/util/List;)V", "getTimestamp", "setTimestamp", "getTitle", "()Ljava/lang/String;", "setTitle", "(Ljava/lang/String;)V", "component1", "component2", "component3", "component4", "component5", "copy", "describeContents", "equals", "", "other", "", "hashCode", "toString", "writeToParcel", "", "parcel", "Landroid/os/Parcel;", "flags", "data-caching_debug"})
+public final class NoteCategory implements android.os.Parcelable {
+    @androidx.room.ColumnInfo(name = "note_category_id")
     private int id;
     @org.jetbrains.annotations.NotNull()
     private java.lang.String title;
-    @org.jetbrains.annotations.Nullable()
-    private uz.behzoddev.data_caching.entities.Note description;
     private long timestamp;
     private long count;
     @org.jetbrains.annotations.NotNull()
-    private java.lang.String type;
-    @org.jetbrains.annotations.Nullable()
     private java.util.List<uz.behzoddev.data_caching.entities.NoteSubCategory> subCategory;
+    public static final android.os.Parcelable.Creator<uz.behzoddev.data_caching.entities.NoteCategory> CREATOR = null;
     
     @org.jetbrains.annotations.NotNull()
     public final uz.behzoddev.data_caching.entities.NoteCategory copy(int id, @org.jetbrains.annotations.NotNull()
-    java.lang.String title, @org.jetbrains.annotations.Nullable()
-    uz.behzoddev.data_caching.entities.Note description, long timestamp, long count, @org.jetbrains.annotations.NotNull()
-    java.lang.String type, @org.jetbrains.annotations.Nullable()
+    java.lang.String title, long timestamp, long count, @org.jetbrains.annotations.NotNull()
     java.util.List<uz.behzoddev.data_caching.entities.NoteSubCategory> subCategory) {
         return null;
     }
@@ -42,10 +44,12 @@ public final class NoteCategory {
         return null;
     }
     
+    public NoteCategory() {
+        super();
+    }
+    
     public NoteCategory(int id, @org.jetbrains.annotations.NotNull()
-    java.lang.String title, @org.jetbrains.annotations.Nullable()
-    uz.behzoddev.data_caching.entities.Note description, long timestamp, long count, @org.jetbrains.annotations.NotNull()
-    java.lang.String type, @org.jetbrains.annotations.Nullable()
+    java.lang.String title, long timestamp, long count, @org.jetbrains.annotations.NotNull()
     java.util.List<uz.behzoddev.data_caching.entities.NoteSubCategory> subCategory) {
         super();
     }
@@ -75,21 +79,7 @@ public final class NoteCategory {
     java.lang.String p0) {
     }
     
-    @org.jetbrains.annotations.Nullable()
-    public final uz.behzoddev.data_caching.entities.Note component3() {
-        return null;
-    }
-    
-    @org.jetbrains.annotations.Nullable()
-    public final uz.behzoddev.data_caching.entities.Note getDescription() {
-        return null;
-    }
-    
-    public final void setDescription(@org.jetbrains.annotations.Nullable()
-    uz.behzoddev.data_caching.entities.Note p0) {
-    }
-    
-    public final long component4() {
+    public final long component3() {
         return 0L;
     }
     
@@ -100,7 +90,7 @@ public final class NoteCategory {
     public final void setTimestamp(long p0) {
     }
     
-    public final long component5() {
+    public final long component4() {
         return 0L;
     }
     
@@ -112,30 +102,47 @@ public final class NoteCategory {
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final java.lang.String component6() {
+    public final java.util.List<uz.behzoddev.data_caching.entities.NoteSubCategory> component5() {
         return null;
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getType() {
-        return null;
-    }
-    
-    public final void setType(@org.jetbrains.annotations.NotNull()
-    java.lang.String p0) {
-    }
-    
-    @org.jetbrains.annotations.Nullable()
-    public final java.util.List<uz.behzoddev.data_caching.entities.NoteSubCategory> component7() {
-        return null;
-    }
-    
-    @org.jetbrains.annotations.Nullable()
     public final java.util.List<uz.behzoddev.data_caching.entities.NoteSubCategory> getSubCategory() {
         return null;
     }
     
-    public final void setSubCategory(@org.jetbrains.annotations.Nullable()
+    public final void setSubCategory(@org.jetbrains.annotations.NotNull()
     java.util.List<uz.behzoddev.data_caching.entities.NoteSubCategory> p0) {
+    }
+    
+    @java.lang.Override()
+    public int describeContents() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public void writeToParcel(@org.jetbrains.annotations.NotNull()
+    android.os.Parcel parcel, int flags) {
+    }
+    
+    @kotlin.Metadata(mv = {1, 6, 0}, k = 3)
+    public static final class Creator implements android.os.Parcelable.Creator<uz.behzoddev.data_caching.entities.NoteCategory> {
+        
+        public Creator() {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        @java.lang.Override()
+        public final uz.behzoddev.data_caching.entities.NoteCategory createFromParcel(@org.jetbrains.annotations.NotNull()
+        android.os.Parcel in) {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        @java.lang.Override()
+        public final uz.behzoddev.data_caching.entities.NoteCategory[] newArray(int size) {
+            return null;
+        }
     }
 }
--- data-caching/build/tmp/kapt3/stubs/debug/uz/behzoddev/data_caching/entities/NoteSubCategory.java
+++ data-caching/build/tmp/kapt3/stubs/debug/uz/behzoddev/data_caching/entities/NoteSubCategory.java
@@ -1,15 +1,18 @@
 package uz.behzoddev.data_caching.entities;
 
-import java.lang.System;
+import android.os.Parcelable;
+import kotlinx.parcelize.Parcelize;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\t\n\u0002\b\u0016\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0086\b\u0018\u00002\u00020\u0001B-\u0012\b\b\u0002\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0005\u0012\b\b\u0002\u0010\u0006\u001a\u00020\u0007\u0012\b\b\u0002\u0010\b\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\tJ\t\u0010\u0018\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0019\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\u001a\u001a\u00020\u0007H\u00c6\u0003J\t\u0010\u001b\u001a\u00020\u0005H\u00c6\u0003J1\u0010\u001c\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00072\b\b\u0002\u0010\b\u001a\u00020\u0005H\u00c6\u0001J\u0013\u0010\u001d\u001a\u00020\u001e2\b\u0010\u001f\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010 \u001a\u00020\u0003H\u00d6\u0001J\t\u0010!\u001a\u00020\u0005H\u00d6\u0001R\u001a\u0010\b\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u000b\"\u0004\b\f\u0010\rR\u001a\u0010\u0002\u001a\u00020\u0003X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000e\u0010\u000f\"\u0004\b\u0010\u0010\u0011R\u001a\u0010\u0006\u001a\u00020\u0007X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0012\u0010\u0013\"\u0004\b\u0014\u0010\u0015R\u001a\u0010\u0004\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0016\u0010\u000b\"\u0004\b\u0017\u0010\r\u00a8\u0006\""}, d2 = {"Luz/behzoddev/data_caching/entities/NoteSubCategory;", "", "id", "", "title", "", "timestamp", "", "description", "(ILjava/lang/String;JLjava/lang/String;)V", "getDescription", "()Ljava/lang/String;", "setDescription", "(Ljava/lang/String;)V", "getId", "()I", "setId", "(I)V", "getTimestamp", "()J", "setTimestamp", "(J)V", "getTitle", "setTitle", "component1", "component2", "component3", "component4", "copy", "equals", "", "other", "hashCode", "toString", "data-caching_debug"})
-public final class NoteSubCategory {
+@kotlinx.parcelize.Parcelize()
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\t\n\u0002\b\u0017\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u0000\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u0087\b\u0018\u00002\u00020\u0001B-\u0012\b\b\u0002\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0004\u001a\u00020\u0005\u0012\b\b\u0002\u0010\u0006\u001a\u00020\u0007\u0012\b\b\u0002\u0010\b\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\tJ\t\u0010\u0018\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0019\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\u001a\u001a\u00020\u0007H\u00c6\u0003J\t\u0010\u001b\u001a\u00020\u0005H\u00c6\u0003J1\u0010\u001c\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00072\b\b\u0002\u0010\b\u001a\u00020\u0005H\u00c6\u0001J\t\u0010\u001d\u001a\u00020\u0003H\u00d6\u0001J\u0013\u0010\u001e\u001a\u00020\u001f2\b\u0010 \u001a\u0004\u0018\u00010!H\u00d6\u0003J\t\u0010\"\u001a\u00020\u0003H\u00d6\u0001J\t\u0010#\u001a\u00020\u0005H\u00d6\u0001J\u0019\u0010$\u001a\u00020%2\u0006\u0010&\u001a\u00020\'2\u0006\u0010(\u001a\u00020\u0003H\u00d6\u0001R\u001a\u0010\b\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u000b\"\u0004\b\f\u0010\rR\u001a\u0010\u0002\u001a\u00020\u0003X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000e\u0010\u000f\"\u0004\b\u0010\u0010\u0011R\u001a\u0010\u0006\u001a\u00020\u0007X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0012\u0010\u0013\"\u0004\b\u0014\u0010\u0015R\u001a\u0010\u0004\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0016\u0010\u000b\"\u0004\b\u0017\u0010\r\u00a8\u0006)"}, d2 = {"Luz/behzoddev/data_caching/entities/NoteSubCategory;", "Landroid/os/Parcelable;", "id", "", "title", "", "timestamp", "", "description", "(ILjava/lang/String;JLjava/lang/String;)V", "getDescription", "()Ljava/lang/String;", "setDescription", "(Ljava/lang/String;)V", "getId", "()I", "setId", "(I)V", "getTimestamp", "()J", "setTimestamp", "(J)V", "getTitle", "setTitle", "component1", "component2", "component3", "component4", "copy", "describeContents", "equals", "", "other", "", "hashCode", "toString", "writeToParcel", "", "parcel", "Landroid/os/Parcel;", "flags", "data-caching_debug"})
+public final class NoteSubCategory implements android.os.Parcelable {
     private int id;
     @org.jetbrains.annotations.NotNull()
     private java.lang.String title;
     private long timestamp;
     @org.jetbrains.annotations.NotNull()
     private java.lang.String description;
+    public static final android.os.Parcelable.Creator<uz.behzoddev.data_caching.entities.NoteSubCategory> CREATOR = null;
     
     @org.jetbrains.annotations.NotNull()
     public final uz.behzoddev.data_caching.entities.NoteSubCategory copy(int id, @org.jetbrains.annotations.NotNull()
@@ -93,5 +96,36 @@ public final class NoteSubCategory {
     
     public final void setDescription(@org.jetbrains.annotations.NotNull()
     java.lang.String p0) {
+    }
+    
+    @java.lang.Override()
+    public int describeContents() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public void writeToParcel(@org.jetbrains.annotations.NotNull()
+    android.os.Parcel parcel, int flags) {
+    }
+    
+    @kotlin.Metadata(mv = {1, 6, 0}, k = 3)
+    public static final class Creator implements android.os.Parcelable.Creator<uz.behzoddev.data_caching.entities.NoteSubCategory> {
+        
+        public Creator() {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        @java.lang.Override()
+        public final uz.behzoddev.data_caching.entities.NoteSubCategory createFromParcel(@org.jetbrains.annotations.NotNull()
+        android.os.Parcel in) {
+            return null;
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        @java.lang.Override()
+        public final uz.behzoddev.data_caching.entities.NoteSubCategory[] newArray(int size) {
+            return null;
+        }
     }
 }
