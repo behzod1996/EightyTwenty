4e8ccf2837094dc84ae2fcf3fdf85698eff943fd
Behod Halil <behzoddev@gmail.com>
2022-07-09 15:29:38 +0900

    Add NoteEntity.kt

---
+++ data-caching/src/main/java/uz/behzoddev/data_caching/entities/notes/NoteEntity.kt
@@ -0,0 +1,27 @@
+package uz.behzoddev.data_caching.entities.notes
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import com.example.core_data.Empty
+import kotlinx.parcelize.Parcelize
+import java.time.ZonedDateTime
+
+@Entity(
+    tableName = "note_table"
+)
+@Parcelize
+data class NoteEntity(
+    @ColumnInfo(name = "note_id")
+    val id: Long = Long.Empty,
+    @ColumnInfo(name = "note_title")
+    val title: String = String.Empty,
+    @ColumnInfo(name = "note_description")
+    val description: String = String.Empty,
+    @ColumnInfo(name = "note_timestamp")
+    val timestamp: ZonedDateTime = ZonedDateTime.now(),
+    @ColumnInfo(name = "note_is_deleted")
+    val isDeleted: Boolean = false,
+    @ColumnInfo(name = "note_category_id")
+    val categoryId: Long = Long.Empty
+): Parcelable
