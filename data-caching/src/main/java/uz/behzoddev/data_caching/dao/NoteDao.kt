398ad0ab35b11bfabeba0e56a015b53c8b0137fc
Behod Halil <behzoddev@gmail.com>
2022-07-11 21:33:52 +0900

    Add NoteDao.kt

---
+++ data-caching/src/main/java/uz/behzoddev/data_caching/dao/NoteDao.kt
@@ -0,0 +1,27 @@
+package uz.behzoddev.data_caching.dao
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.data_caching.entities.notes.NoteEntity
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: NoteEntity): Long
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateNote(note: NoteEntity): Long
+
+    @Delete
+    suspend fun delete(note: NoteEntity): Long
+
+    @Transaction
+    @Query("SELECT * FROM note_table WHERE note_is_trashed = 1")
+    fun fetchTrashedNotes(): Flow<List<NoteEntity>>
+
+    @Transaction
+    @Query("SELECT * FROM note_table WHERE note_is_trashed <> 1")
+    fun fetchAllNotes(): Flow<List<NoteEntity>>
+
+}
