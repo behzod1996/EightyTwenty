a4a94eba719e70bb7bc130c0c3206cc7b993e91c
Behod Halil <behzoddev@gmail.com>
2022-07-12 22:19:13 +0900

    [feat::] Update NoteCategoryDao.kt

--- data-caching/src/main/java/uz/behzoddev/data_caching/dao/NoteCategoryDao.kt
+++ data-caching/src/main/java/uz/behzoddev/data_caching/dao/NoteCategoryDao.kt
@@ -1,19 +1,17 @@
 package uz.behzoddev.data_caching.dao
 
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 import uz.behzoddev.data_caching.entities.notes.NoteCategoryEntity
 
+@Dao
 interface NoteCategoryDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNoteCategory(noteCategory: NoteCategoryEntity): Long
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun updateNoteCategory(noteCategory: NoteCategoryEntity): Long
+    suspend fun updateNoteCategory(noteCategory: NoteCategoryEntity)
 
     @Query("UPDATE note_category_table SET note_count = note_count + 1 WHERE id = :noteCategoryId")
     suspend fun incrementNoteCount(noteCategoryId: Long)
