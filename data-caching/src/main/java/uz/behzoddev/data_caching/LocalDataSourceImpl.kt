2c7684643e17d5d0400fff54be77bc6f02b4774e
Behod Halil <behzoddev@gmail.com>
2022-07-12 20:38:01 +0900

    [feat::] Add LocalDataSourceImpl.kt

---
+++ data-caching/src/main/java/uz/behzoddev/data_caching/LocalDataSourceImpl.kt
@@ -0,0 +1,58 @@
+package uz.behzoddev.data_caching
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.data_caching.dao.NoteCategoryDao
+import uz.behzoddev.data_caching.dao.NoteDao
+import uz.behzoddev.data_caching.entities.notes.NoteCategoryEntity
+import uz.behzoddev.data_caching.entities.notes.NoteEntity
+import javax.inject.Inject
+
+class LocalDataSourceImpl @Inject constructor(
+    private val noteCategoryDao: NoteCategoryDao,
+    private val noteDao: NoteDao
+) : LocalDataSource {
+
+    override suspend fun insertNoteCategory(noteCategory: NoteCategoryEntity): Long {
+        return noteCategoryDao.insertNoteCategory(noteCategory = noteCategory)
+    }
+
+    override suspend fun updateNoteCategory(noteCategory: NoteCategoryEntity): Long {
+        return noteCategoryDao.updateNoteCategory(noteCategory = noteCategory)
+    }
+
+    override suspend fun incrementNoteCount(noteCategoryId: Long) {
+        return noteCategoryDao.incrementNoteCount(noteCategoryId = noteCategoryId)
+    }
+
+    override suspend fun decrementNoteCount(noteCategoryId: Long) {
+       return noteCategoryDao.decrementNoteCount(noteCategoryId = noteCategoryId)
+    }
+
+    override fun fetchAllCategories(): Flow<List<NoteCategoryEntity>> {
+        return noteCategoryDao.fetchAllCategories()
+    }
+
+    override suspend fun fetchIfCategoryIdExists(id: Int): Boolean {
+        return noteCategoryDao.fetchIfCategoryIdExists(id)
+    }
+
+    override suspend fun insertNote(note: NoteEntity): Long {
+        return noteDao.insertNote(note = note)
+    }
+
+    override suspend fun updateNote(note: NoteEntity): Long {
+        return noteDao.updateNote(note = note)
+    }
+
+    override suspend fun delete(note: NoteEntity): Long {
+        return noteDao.delete(note = note)
+    }
+
+    override fun fetchTrashedNotes(): Flow<List<NoteEntity>> {
+        return noteDao.fetchTrashedNotes()
+    }
+
+    override fun fetchAllNotes(): Flow<List<NoteEntity>> {
+        return noteDao.fetchAllNotes()
+    }
+}
