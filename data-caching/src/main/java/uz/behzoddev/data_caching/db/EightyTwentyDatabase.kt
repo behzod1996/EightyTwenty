8a33c6b8f16c16bf5364b66086c3a4378d3ac48f
Behod Halil <behzoddev@gmail.com>
2022-07-12 20:37:44 +0900

    [feat::] EightyTwentyDatabase.kt

---
+++ data-caching/src/main/java/uz/behzoddev/data_caching/db/EightyTwentyDatabase.kt
@@ -0,0 +1,38 @@
+package uz.behzoddev.data_caching.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import uz.behzoddev.data_caching.dao.NoteCategoryDao
+import uz.behzoddev.data_caching.dao.NoteDao
+import uz.behzoddev.data_caching.entities.notes.NoteCategoryEntity
+import uz.behzoddev.data_caching.entities.notes.NoteEntity
+
+@Database(
+    entities = [NoteCategoryEntity::class,NoteEntity::class],
+    version = 1
+)
+abstract class EightyTwentyDatabase : RoomDatabase() {
+
+    abstract fun getNoteCategoryDao(): NoteCategoryDao
+    abstract fun getNoteDao(): NoteDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: EightyTwentyDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = INSTANCE ?: synchronized(LOCK) {
+            INSTANCE ?: createDatabase(context).also { INSTANCE = it }
+        }
+
+        private fun createDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            EightyTwentyDatabase::class.java,
+            "eighty_twenty_database"
+        ).build()
+
+    }
+}
