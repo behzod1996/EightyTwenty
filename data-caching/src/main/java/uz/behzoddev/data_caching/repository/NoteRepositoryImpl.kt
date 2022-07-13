40dc88d8b8c0ffedeee238abbe01192a87f1ee80
Behod Halil <behzoddev@gmail.com>
2022-07-13 20:58:41 +0900

    [feat::] Update NoteRepositoryImpl.kt

--- data-caching/src/main/java/uz/behzoddev/data_caching/repository/NoteRepositoryImpl.kt
+++ data-caching/src/main/java/uz/behzoddev/data_caching/repository/NoteRepositoryImpl.kt
@@ -3,27 +3,32 @@ package uz.behzoddev.data_caching.repository
 import com.example.domain.models.NoteDomainModel
 import com.example.domain.repository.NoteRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import uz.behzoddev.data_caching.LocalDataSource
+import uz.behzoddev.data_caching.mapper.NoteMapper
 import javax.inject.Inject
 
 class NoteRepositoryImpl @Inject constructor(
-    private val source: LocalDataSource
-): NoteRepository {
+    private val source: LocalDataSource,
+    private val noteMapper: NoteMapper
+) : NoteRepository {
 
     override suspend fun insertNote(note: NoteDomainModel): Long {
-       TODO()
+        return source.insertNote(noteMapper.toEntity(note))
     }
 
     override suspend fun updateNote(note: NoteDomainModel) {
-        TODO("Not yet implemented")
+        return source.updateNote(noteMapper.toEntity(note))
     }
 
-    override suspend fun delete(note: NoteDomainModel){
-        TODO("Not yet implemented")
+    override suspend fun delete(note: NoteDomainModel) {
+        return source.delete(noteMapper.toEntity(note))
     }
 
     override fun fetchTrashedNotes(): Flow<List<NoteDomainModel>> {
-        TODO("Not yet implemented")
+        return flow {
+
+        }
     }
 
     override fun fetchAllNotes(): Flow<List<NoteDomainModel>> {
