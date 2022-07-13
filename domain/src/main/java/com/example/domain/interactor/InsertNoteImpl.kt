8ac17a17ebbd89d34a0cf529b1acde773a01c7d6
Behod Halil <behzoddev@gmail.com>
2022-07-13 20:58:15 +0900

    [feat::] Add InsertNoteImpl.kt

---
+++ domain/src/main/java/com/example/domain/interactor/InsertNoteImpl.kt
@@ -0,0 +1,10 @@
+package com.example.domain.interactor
+
+import com.example.domain.models.NoteDomainModel
+
+class InsertNoteImpl : InsertNote {
+
+    override suspend fun invoke(note: NoteDomainModel): Long {
+        TODO("Not yet implemented")
+    }
+}
