4dfe06ec5395b0dbc59dee084e9a53976472bd44
Behod Halil <behzoddev@gmail.com>
2022-07-15 21:58:25 +0900

    [feat::] Update NoteRepositoryImpl.kt

--- data-caching/src/main/java/uz/behzoddev/data_caching/repository/NoteRepositoryImpl.kt
+++ data-caching/src/main/java/uz/behzoddev/data_caching/repository/NoteRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package uz.behzoddev.data_caching.repository
 
+import com.example.core_data.Resource
+import com.example.core_data.execute
 import com.example.domain.models.NoteDomainModel
 import com.example.domain.repository.NoteRepository
 import kotlinx.coroutines.flow.Flow
@@ -25,13 +27,49 @@ class NoteRepositoryImpl @Inject constructor(
         return source.delete(noteMapper.toEntity(note))
     }
 
-    override fun fetchTrashedNotes(): Flow<List<NoteDomainModel>> {
+    override fun fetchTrashedNotes(): Flow<Resource<List<NoteDomainModel>>> {
         return flow {
+            val result = execute {
+                source.fetchTrashedNotes().map { noteMapper.toDomain(it) }
+            }
 
+            when(result) {
+                is Resource.Empty -> {
+                    emit(Resource.Empty())
+                }
+                is Resource.Failure -> {
+                    emit(Resource.Failure(result.message!!))
+                }
+                is Resource.Loading -> {
+                    emit(Resource.Loading())
+                }
+                is Resource.Success -> {
+                    emit(Resource.Success(data = result.data!!))
+                }
+            }
         }
     }
 
-    override fun fetchAllNotes(): Flow<List<NoteDomainModel>> {
-        TODO("Not yet implemented")
+    override fun fetchAllNotes(): Flow<Resource<List<NoteDomainModel>>> {
+        return flow {
+            val result = execute {
+                source.fetchAllNotes().map { noteMapper.toDomain(it) }
+            }
+
+            when(result) {
+                is Resource.Empty -> {
+                    emit(Resource.Empty())
+                }
+                is Resource.Failure -> {
+                    emit(Resource.Failure(result.message!!))
+                }
+                is Resource.Loading -> {
+                    emit(Resource.Loading())
+                }
+                is Resource.Success -> {
+                    emit(Resource.Success(data = result.data!!))
+                }
+            }
+        }
     }
 }
