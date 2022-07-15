348674b4068eb6a205bd70cc41c59ac75b1d3b41
Behzod Halil <72313937+behzod1996@users.noreply.github.com>
2022-07-15 22:03:48 +0900

    Delete NewNoteNavigator.java

--- feature-new-note/build/tmp/kapt3/stubs/debug/uz/behzoddev/feature_new_note/NewNoteNavigator.java
+++
@@ -1,7 +0,0 @@
-package uz.behzoddev.feature_new_note;
-
-import uz.behzoddev.common_ui.core.Navigator;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\u00020\u0001\u00a8\u0006\u0002"}, d2 = {"Luz/behzoddev/feature_new_note/NewNoteNavigator;", "Luz/behzoddev/common_ui/core/Navigator;", "feature-new-note_debug"})
-public abstract interface NewNoteNavigator extends uz.behzoddev.common_ui.core.Navigator {
-}
