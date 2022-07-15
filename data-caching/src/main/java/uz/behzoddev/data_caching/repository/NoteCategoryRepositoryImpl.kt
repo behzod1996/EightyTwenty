18acdfaeb58597310a1f85e53c16fab81e77986f
Behod Halil <behzoddev@gmail.com>
2022-07-15 21:58:08 +0900

    [feat::] Update NoteCategoryRepositoryImpl.kt

--- data-caching/src/main/java/uz/behzoddev/data_caching/repository/NoteCategoryRepositoryImpl.kt
+++ data-caching/src/main/java/uz/behzoddev/data_caching/repository/NoteCategoryRepositoryImpl.kt
@@ -1,4 +1,44 @@
 package uz.behzoddev.data_caching.repository
 
-class NoteCategoryRepositoryImpl {
+import com.example.core_data.Resource
+import com.example.domain.models.NoteCategoryDomainModel
+import com.example.domain.repository.NoteCategoryRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import uz.behzoddev.data_caching.LocalDataSource
+import uz.behzoddev.data_caching.mapper.NoteCategoryMapper
+import javax.inject.Inject
+
+class NoteCategoryRepositoryImpl @Inject constructor(
+    private val source: LocalDataSource,
+    private val mapper: NoteCategoryMapper
+) : NoteCategoryRepository {
+
+    override suspend fun insertNoteCategory(noteCategory: NoteCategoryDomainModel): Long {
+        return source.insertNoteCategory(mapper.toEntity(domain = noteCategory))
+    }
+
+    override suspend fun updateNoteCategory(noteCategory: NoteCategoryDomainModel) {
+        return source.updateNoteCategory(mapper.toEntity(domain = noteCategory))
+    }
+
+    override suspend fun incrementNoteCount(noteCategoryId: Long) {
+        return source.incrementNoteCount(noteCategoryId)
+    }
+
+    override suspend fun decrementNoteCount(noteCategoryId: Long) {
+        return source.decrementNoteCount(noteCategoryId)
+    }
+
+    override fun fetchAllCategories(): Flow<Resource<List<NoteCategoryDomainModel>>> {
+        return flow {
+            
+        }
+    }
+
+    override suspend fun fetchIfCategoryIdExists(id: Int): Boolean {
+        TODO("Not yet implemented")
+    }
 }
