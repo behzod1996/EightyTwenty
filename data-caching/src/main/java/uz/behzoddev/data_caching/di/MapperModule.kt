2ec22775d0c23de473fa091b3b8b1e12e20bd4d4
Behod Halil <behzoddev@gmail.com>
2022-07-19 22:03:15 +0900

    [feat::] Update MapperModule.kt

--- data-caching/src/main/java/uz/behzoddev/data_caching/di/MapperModule.kt
+++ data-caching/src/main/java/uz/behzoddev/data_caching/di/MapperModule.kt
@@ -6,6 +6,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import uz.behzoddev.data_caching.mapper.NoteCategoryMapper
 import uz.behzoddev.data_caching.mapper.NoteMapper
+import uz.behzoddev.data_caching.mapper.NotesMapper
 import javax.inject.Singleton
 
 @Module
@@ -22,5 +23,13 @@ object MapperModule {
     @Singleton
     fun providesNoteCategoryMapper(): NoteCategoryMapper {
         return NoteCategoryMapper.create()
+    }
+
+    @Provides
+    @Singleton
+    fun providesNotesMapper(
+        noteMapper: NoteMapper
+    ): NotesMapper {
+        return NotesMapper(noteMapper)
     }
 }
